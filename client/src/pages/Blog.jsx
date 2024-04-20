e45122eb50eea2530c6aa14fc91d80ccb5861d86
Ashish Gupta <145846915+Ashish90278@users.noreply.github.com>
2024-04-21 00:57:08 +0530

    Update Blog.jsx
    Changes in img file path

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -6,12 +6,12 @@ export const Blog = () => {
     <>
       <div className="blogContainer container">
         <div className="Section">
-          <BlogCard blogImage="../../public/imgs/Blog1.jpeg" blogTopic="Everthing about Tesla entering India" />
-          <BlogCard blogImage="../../public/imgs/Blog2.jpeg" blogTopic="The humongous scale of Apple" />
-          <BlogCard blogImage="../../public/imgs/Blog3.jpeg" blogTopic="How Decathlon is outcompeting giants" />
-          <BlogCard blogImage="../../public/imgs/Blog4.jpeg" blogTopic="India's own financial powerhouse: GIFT CITY" />
-          <BlogCard blogImage="../../public/imgs/Blog5.jpeg" blogTopic="How Yahoo! failed to buy Google(twice!)" />
-          <BlogCard blogImage="../../public/imgs/Blog6.jpeg" blogTopic="ZARA's Secret to Its Popularity in India" />
+          <BlogCard blogImage="../imgs/Blog1.jpeg" blogTopic="Everthing about Tesla entering India" />
+          <BlogCard blogImage="../imgs/Blog2.jpeg" blogTopic="The humongous scale of Apple" />
+          <BlogCard blogImage="../imgs/Blog3.jpeg" blogTopic="How Decathlon is outcompeting giants" />
+          <BlogCard blogImage="../imgs/Blog4.jpeg" blogTopic="India's own financial powerhouse: GIFT CITY" />
+          <BlogCard blogImage="../imgs/Blog5.jpeg" blogTopic="How Yahoo! failed to buy Google(twice!)" />
+          <BlogCard blogImage="../imgs/Blog6.jpeg" blogTopic="ZARA's Secret to Its Popularity in India" />
         </div>
       </div>
       <hr />

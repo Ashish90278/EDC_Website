e5501c13f1dd7c75d21a46f4c432a064cf0d9a3f
Ashish90278 <ashishgupta.90278@gmail.com>
2024-05-06 03:11:48 +0530

    connect to backend

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://www.edcjss.com/',
     methods:"GET, POST, DELETE, PUT, PATCH, HEAD",
     creadentials: true,
 }
